28a56eb35ff4978d432ae1e03182373a6472bf08
Minh Nguyen <116859668+cong-minhh@users.noreply.github.com>
2025-10-16 10:36:15 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['typing-thunder-game.onrender.com'],
+    port: 3000,
+  },
 })
